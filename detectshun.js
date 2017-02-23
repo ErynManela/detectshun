f59617332544c3af4b8867be85f832002e6a6cfd
Aaron Manela <amanela@overdrive.com>
2017-02-23 14:02:14 -0500

    Fixing exports

--- detectshun.js
+++ detectshun.js
@@ -12,56 +12,54 @@
     }
 }(this, function (exports, bowser) {
 
+    // attach properties to the exports object to define
+    // the exported module properties.
 
-    var detectshun = {
-        feature: function(needs, callback) {
-            var supported = document.getElementsByTagName('html')[0].className.split(/\s+/);
-            var score = 0;
+    exports.feature = function(needs, callback) {
+        var supported = document.getElementsByTagName('html')[0].className.split(/\s+/);
+        var score = 0;
 
-            for (var i = 0; i < needs.length; i++) {
-                var has = supported.indexOf(needs[i]);
-                if (has != -1) {
-                    score++;
-                }
+        for (var i = 0; i < needs.length; i++) {
+            var has = supported.indexOf(needs[i]);
+            if (has != -1) {
+                score++;
             }
+        }
 
-            if (score < 1) {
-                console.log("WARNING: detectshun requires modernizr 2.x or higher to be included in your project for feature checks to work");
+        if (score < 1) {
+            console.log("WARNING: detectshun requires modernizr 2.x or higher to be included in your project for feature checks to work");
+            callback(false);
+        } else {
+            if (score == needs.length) {
+                callback(true);
+            } else {
                 callback(false);
-            } else {
-                if (score == needs.length) {
-                    callback(true);
-                } else {
-                    callback(false);
-                }
             }
-        },
-
-        browser: function() {
-            var grade = null;
-
-            if(bowser.a) {
-                grade = "a";
-            } else if(bowser.c) {
-                grade = "c";
-            } else {
-                grade = "x";
-            }
-
-            return {
-                name: bowser.name,
-                version: bowser.version,
-                grade: grade,
-                mobile: bowser.mobile || false,
-                tablet: bowser.tablet || false,
-                ios: bowser.ios || false,
-                android: bowser.android || false,
-                bowser: bowser
-            };
         }
     };
 
-    // attach properties to the exports object to define
-    // the exported module properties.
-    exports = detectshun;
+    exports.browser = function() {
+        var grade = null;
+
+        if(bowser.a) {
+            grade = "a";
+        } else if(bowser.c) {
+            grade = "c";
+        } else {
+            grade = "x";
+        }
+
+        return {
+            name: bowser.name,
+            version: bowser.version,
+            grade: grade,
+            mobile: bowser.mobile || false,
+            tablet: bowser.tablet || false,
+            ios: bowser.ios || false,
+            android: bowser.android || false,
+            bowser: bowser
+        };
+    };
+
+
 }));
